cf7cc273bd2cf9f14177f648f30f4372c234ebbc
Alan Krug <krugalan@gmail.com>
2025-08-31 22:34:54 -0300

    update

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: 'https://natalia-dev-rgb.github.io/Rio-Spa/',
+  base: '/Rio-Spa/',
   plugins: [react()],
 })
